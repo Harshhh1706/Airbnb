60c4b5224f986a0a622e03545b0662f451cac50f
harshwardhan <harshvardhanpalange@gmail.com>
2026-08-22 23:18:57 +0530

    Added listings and ejs templates

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Listing = require("./models/Listing.js");
-const Data = require("./data.js");
+const initData = require("./data.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
